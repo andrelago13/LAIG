fd8ba0f3e2bee7eea44fa4d756377072ad8b0f9c
aslmbc13 <asl_mbc_13@hotmail.com>
2015-09-25 16:20:32 +0100

    Libs e reader

---
+++ LAIG TP1/reader/MySceneGraph.js
@@ -0,0 +1,97 @@
+
+function MySceneGraph(filename, scene) {
+	this.loadedOk = null;
+	
+	// Establish bidirectional references between scene and graph
+	this.scene = scene;
+	scene.graph=this;
+		
+	// File reading 
+	this.reader = new CGFXMLreader();
+
+	/*
+	 * Read the contents of the xml file, and refer to this class for loading and error handlers.
+	 * After the file is read, the reader calls onXMLReady on this object.
+	 * If any error occurs, the reader calls onXMLError on this object, with an error message
+	 */
+	 
+	this.reader.open('scenes/'+filename, this);  
+}
+
+/*
+ * Callback to be executed after successful reading
+ */
+MySceneGraph.prototype.onXMLReady=function() 
+{
+	console.log("XML Loading finished.");
+	var rootElement = this.reader.xmlDoc.documentElement;
+	
+	// Here should go the calls for different functions to parse the various blocks
+	var error = this.parseGlobalsExample(rootElement);
+
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}	
+
+	this.loadedOk=true;
+	
+	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
+	this.scene.onGraphLoaded();
+};
+
+
+
+/*
+ * Example of method that parses elements of one block and stores information in a specific data structure
+ */
+MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
+	
+	var elems =  rootElement.getElementsByTagName('globals');
+	if (elems == null) {
+		return "globals element is missing.";
+	}
+
+	if (elems.length != 1) {
+		return "either zero or more than one 'globals' element found.";
+	}
+
+	// various examples of different types of access
+	var globals = elems[0];
+	this.background = this.reader.getRGBA(globals, 'background');
+	this.drawmode = this.reader.getItem(globals, 'drawmode', ["fill","line","point"]);
+	this.cullface = this.reader.getItem(globals, 'cullface', ["back","front","none", "frontandback"]);
+	this.cullorder = this.reader.getItem(globals, 'cullorder', ["ccw","cw"]);
+
+	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
+
+	var tempList=rootElement.getElementsByTagName('list');
+
+	if (tempList == null  || tempList.length==0) {
+		return "list element is missing.";
+	}
+	
+	this.list=[];
+	// iterate over every element
+	var nnodes=tempList[0].children.length;
+	for (var i=0; i< nnodes; i++)
+	{
+		var e=tempList[0].children[i];
+
+		// process each element and store its information
+		this.list[e.id]=e.attributes.getNamedItem("coords").value;
+		console.log("Read list item id "+ e.id+" with value "+this.list[e.id]);
+	};
+
+};
+	
+/*
+ * Callback to be executed on any read error
+ */
+ 
+MySceneGraph.prototype.onXMLError=function (message) {
+	console.error("XML Loading Error: "+message);	
+	this.loadedOk=false;
+};
+
+
---
+++ LAIG TP1/reader/XMLscene.js
@@ -0,0 +1,89 @@
+
+function XMLscene() {
+    CGFscene.call(this);
+}
+
+XMLscene.prototype = Object.create(CGFscene.prototype);
+XMLscene.prototype.constructor = XMLscene;
+
+XMLscene.prototype.init = function (application) {
+    CGFscene.prototype.init.call(this, application);
+
+    this.initCameras();
+
+    this.initLights();
+
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    this.gl.clearDepth(100.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+	this.gl.enable(this.gl.CULL_FACE);
+    this.gl.depthFunc(this.gl.LEQUAL);
+
+	this.axis=new CGFaxis(this);
+};
+
+XMLscene.prototype.initLights = function () {
+
+    this.shader.bind();
+
+	this.lights[0].setPosition(2, 3, 3, 1);
+    this.lights[0].setDiffuse(1.0,1.0,1.0,1.0);
+    this.lights[0].update();
+ 
+    this.shader.unbind();
+};
+
+XMLscene.prototype.initCameras = function () {
+    this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
+};
+
+XMLscene.prototype.setDefaultAppearance = function () {
+    this.setAmbient(0.2, 0.4, 0.8, 1.0);
+    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+    this.setSpecular(0.2, 0.4, 0.8, 1.0);
+    this.setShininess(10.0);	
+};
+
+// Handler called when the graph is finally loaded. 
+// As loading is asynchronous, this may be called already after the application has started the run loop
+XMLscene.prototype.onGraphLoaded = function () 
+{
+	this.gl.clearColor(this.graph.background[0],this.graph.background[1],this.graph.background[2],this.graph.background[3]);
+	this.lights[0].setVisible(true);
+    this.lights[0].enable();
+};
+
+XMLscene.prototype.display = function () {
+	// ---- BEGIN Background, camera and axis setup
+    this.shader.bind();
+	
+	// Clear image and depth buffer everytime we update the scene
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+	// Initialize Model-View matrix as identity (no transformation
+	this.updateProjectionMatrix();
+    this.loadIdentity();
+
+	// Apply transformations corresponding to the camera position relative to the origin
+	this.applyViewMatrix();
+
+	// Draw axis
+	this.axis.display();
+
+	this.setDefaultAppearance();
+	
+	// ---- END Background, camera and axis setup
+
+	// it is important that things depending on the proper loading of the graph
+	// only get executed after the graph has loaded correctly.
+	// This is one possible way to do it
+	if (this.graph.loadedOk)
+	{
+		this.lights[0].update();
+	};	
+
+    this.shader.unbind();
+};
+
