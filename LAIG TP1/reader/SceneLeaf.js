ff2ed19cb37f1fcba94bd28e5291e6a9868f4b81
aslmbc13 <asl_mbc_13@hotmail.com>
2015-10-09 17:48:47 +0100

    Changed leaf creation

--- LAIG TP1/reader/SceneLeaf.js
+++ LAIG TP1/reader/SceneLeaf.js
@@ -1,7 +1,11 @@
-/*SceneLeaf.protoype = new SceneNode();
+SceneLeaf.protoype = new SceneNode();
 SceneLeaf.constructor = SceneLeft;
 
 function SceneLeaf(primitive) {
 	this.m = mat4.create();
 	this.primitive = primitive;
-}*/
+}
+
+SceneLeaf.prototype.display = function () {
+	this.primitive.display();
+}
