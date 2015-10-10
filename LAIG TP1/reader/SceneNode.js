f1a29942441286094ee23761257663b81eb3ced3
gtugablue <gtugablue@gmail.com>
2015-10-10 17:29:55 +0100

    Texture display fixed.

--- LAIG TP1/reader/SceneNode.js
+++ LAIG TP1/reader/SceneNode.js
@@ -27,16 +27,18 @@ SceneNode.prototype.display = function() {
 
 	this.scene.pushMatrix();
 	this.scene.multMatrix(this.m);
+	var currTexture = this.scene.activeTexture;
 	for(var i = 0; i < this.descendants.length; i++) {
-		if (this.material != null) this.material.apply();
+		if (this.material !== null) this.material.apply();
 		if (this.texture === "clear")
 		{
 			if (this.scene.activeTexture !== null)
 				this.scene.activeTexture.unbind();
 		}
-		else if (this.texture != null) this.texture.bind();
+		else if (this.texture !== null) this.texture.bind();
 		this.descendants[i].display();
 	}
+	if (currTexture !== null) currTexture.bind();
 	this.scene.popMatrix();
 }
 
