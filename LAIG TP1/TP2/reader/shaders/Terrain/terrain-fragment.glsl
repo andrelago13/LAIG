200548e96d45932839cfcf11164f7356c905a038
gtugablue <gtugablue@gmail.com>
2015-11-10 16:24:27 +0000

    Shader.

--- LAIG TP1/TP2/reader/shaders/Terrain/terrain-fragment.glsl
+++ LAIG TP1/TP2/reader/shaders/Terrain/terrain-fragment.glsl
@@ -1,5 +1,9 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
 varying vec2 vTextureCoord;
-uniform sampler2D uSampler
+uniform sampler2D uSampler;
 
 void main()
 {
--- LAIG TP1/TP2/reader/shaders/Terrain/terrain-vertex.glsl
+++ LAIG TP1/TP2/reader/shaders/Terrain/terrain-vertex.glsl
@@ -6,42 +6,12 @@ mat4 uMVMatrix;
 mat4 uPMatrix;
 mat4 uNMatrix;
 
-vec4 uGlobalAmbient;
-
-struct lightProperties {
-    vec4 position;                  // Default: (0, 0, 1, 0)
-    vec4 ambient;                   // Default: (0, 0, 0, 1)
-    vec4 diffuse;                   // Default: (0, 0, 0, 1)
-    vec4 specular;                  // Default: (0, 0, 0, 1)
-    vec4 half_vector;
-    vec3 spot_direction;            // Default: (0, 0, -1)
-    float spot_exponent;            // Default: 0 (possible values [0, 128]
-    float spot_cutoff;              // Default: 180 (possible values [0, 90] or 180)
-    float constant_attenuation;     // Default: 1 (value must be >= 0)
-    float linear_attenuation;       // Default: 0 (value must be >= 0)
-    float quadratic_attenuation;    // Default: 0 (value must be >= 0)
-    bool enabled;                   // Deafult: false
-};
-
-struct materialProperties {
-    vec4 ambient;                   // Default: (0, 0, 0, 1)
-    vec4 diffuse;                   // Default: (0, 0, 0, 1)
-    vec4 specular;                  // Default: (0, 0, 0, 1)
-    vec4 emission;                  // Default: (0, 0, 0, 1)
-    float shininess;                // Default: 0 (possible values [0, 128])
-};
-
-#define NUMBER_OF_LIGHTS 8
-lightProperties uLight[NUMBER_OF_LIGHTS];
-
-materialProperties uFrontMaterial;
-materialProperties uBackMaterial;
-
-bool uUseTexture;
-
 varying vec2 vTextureCoord;
+
+attribute vec3 aHeight;
 
 void main()
 {
-	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition + aHeight, 1.0);
+	vTextureCoord = aTextureCoord;
 }
