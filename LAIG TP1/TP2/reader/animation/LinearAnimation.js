960f0488a1c33ea8cff46a57ade516ac688cb076
gtugablue <gtugablue@gmail.com>
2015-11-10 01:08:07 +0000

    Linear Animation finished.

--- LAIG TP1/TP2/reader/animation/LinearAnimation.js
+++ LAIG TP1/TP2/reader/animation/LinearAnimation.js
@@ -18,23 +18,32 @@ LinearAnimation.prototype.getMatrix = function(t) {
 	if (t > this.span)
 	{
 		mat4.translate(m, m, this.controlPoints[this.controlPoints.length - 1]);
+		mat4.rotate(m, m, this.calculateRotation(this.controlPoints[this.controlPoints.length - 2], this.controlPoints[this.controlPoints.length - 1]), [0, 1, 0]);
 		return m;
 	}
 	var totalS = this.totalDistance * t / this.span;
 	var currentDist = 0;
 	var i;
+	var dist;
 	for (i = 1; i < this.controlPoints.length; i++)
 	{
-		var dist = this.calculateDistance(this.controlPoints[i - 1], this.controlPoints[i]);
+		dist = this.calculateDistance(this.controlPoints[i - 1], this.controlPoints[i]);
 		if (currentDist + dist < totalS)
 			currentDist += dist;
 		else
 			break;
 	}
 	var s = totalS - currentDist;
-	var interp = this.lerp(this.controlPoints[i - 1], this.controlPoints[i], t / this.span);
+	var interp = this.lerp(this.controlPoints[i - 1], this.controlPoints[i], s / dist);
 	mat4.translate(m, m, interp);
+
+	mat4.rotate(m, m, this.calculateRotation(this.controlPoints[i - 1], this.controlPoints[i]), [0, 1, 0]);
+
 	return m;
+}
+
+LinearAnimation.prototype.calculateRotation = function(p1, p2) {
+	return Math.atan2(p2[0] - p1[0], p2[2] - p1[2]);
 }
 
 LinearAnimation.prototype.calculateDistance = function(p1, p2) {
