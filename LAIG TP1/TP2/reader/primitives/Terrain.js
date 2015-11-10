7ed6ba57e4e04e5bef522117e22fb41e93864e02
gtugablue <gtugablue@gmail.com>
2015-11-10 21:02:54 +0000

    ...

--- LAIG TP1/TP2/reader/primitives/Terrain.js
+++ LAIG TP1/TP2/reader/primitives/Terrain.js
@@ -4,11 +4,15 @@ function Terrain(scene, texture, heightmap) {
 	this.initBuffers();
 	this.texture = texture;
 	this.heightmap = heightmap;
+	
+	this.shader = new CGFshader(this.scene.gl, "../shaders/Terrain/terrain-vertex.glsl", "../shaders/Terrain/terrain-fragment.glsl");
 };
 
 Terrain.prototype = Object.create(CGFobject.prototype);
 Terrain.prototype.constructor=Terrain;
 
 Terrain.prototype.display = function() {
+	this.scene.setActiveShader(this.shader);
 	
+	this.scene.setActiveShader(this.scene.defaultShader);
 }
