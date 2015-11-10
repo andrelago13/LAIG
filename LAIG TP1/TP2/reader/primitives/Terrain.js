38bfc7eb6aee29efd532c754e1cf1745865ff06e
gtugablue <gtugablue@gmail.com>
2015-11-10 16:37:43 +0000

    ...

--- LAIG TP1/TP2/reader/primitives/Terrain.js
+++ LAIG TP1/TP2/reader/primitives/Terrain.js
@@ -1,8 +1,14 @@
-function Terrain(scene) {
+function Terrain(scene, texture, heightmap) {
 	CGFobject.call(this,scene);
 
 	this.initBuffers();
+	this.texture = texture;
+	this.heightmap = heightmap;
 };
 
 Terrain.prototype = Object.create(CGFobject.prototype);
 Terrain.prototype.constructor=Terrain;
+
+Terrain.prototype.display = function() {
+	
+}
