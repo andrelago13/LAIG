f9cf81fffa347ef66b79d2833bfb76de86a6792a
Gustavo Silva <gtugablue@gmail.com>
2015-12-19 15:23:09 +0000

    ...

--- LAIG TP1/TP3/reader/main.js
+++ LAIG TP1/TP3/reader/main.js
@@ -36,7 +36,7 @@ serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'SceneNode.js'
                'game/Modx.js', 
                'game/State.js',
                'game/StateWaitingForPlay.js', 
-               'game/StateMakingPlay.js', 
+               'game/StatePlacingXPiece.js', 
                'scenarios/Scenario.js', 
                'scenarios/MoonLandingScenario.js', 
                'scenarios/AfternoonSkyScenario.js', 
