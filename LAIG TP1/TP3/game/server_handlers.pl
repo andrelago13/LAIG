9c8eda08235e4b6cae67d13fa4e0d677b8395a10
aslmbc13 <asl_mbc_13@hotmail.com>
2015-12-20 00:58:28 +0000

    Next player bug fix

--- LAIG TP1/TP3/game/server_handlers.pl
+++ LAIG TP1/TP3/game/server_handlers.pl
@@ -8,8 +8,12 @@ parse_input(quit, goodbye).
 parse_input(start_game(X,Y), Game) :- start_game(Game, X, Y).
 parse_input(start_game(_,_), error_starting_game).
 parse_input(make_play(Game, X, Y), NewGame) :-
+        game_board(Game, Board),
+        num_jokers_to_place(Board, 0),
         place_xpiece(Game, [X, Y], Game1),
         end_play(Game1, NewGame).
+parse_input(make_play(Game, X, Y), NewGame) :-
+        place_xpiece(Game, [X, Y], NewGame).
 parse_input(make_play(_, _, _), error_invalid_piece).
 parse_input(num_jokers_to_place(Game), N) :-
         game_board(Game, Board),
