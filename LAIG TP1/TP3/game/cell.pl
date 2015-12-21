b05a50301aaa86fa42e907dd3e71ec79ff54e742
aslmbc13 <asl_mbc_13@hotmail.com>
2015-12-21 18:09:49 +0000

    Bugfix

--- LAIG TP1/TP3/game/cell.pl
+++ LAIG TP1/TP3/game/cell.pl
@@ -7,10 +7,10 @@ cell_top_spiece(Cell, S) :-
         S = -1.
 cell_top_spiece(Cell, S) :-
         cell_spieces(Cell, [S | _]).
-cell_convert_xpiece_to_spiece([Ss, -1], [Ss, -1]).
-cell_convert_xpiece_to_spiece([Ss, 0], [Ss, -1]).
-cell_convert_xpiece_to_spiece([[], X], [[X], -1]).
-cell_convert_xpiece_to_spiece([[X | Ss], X], [[X | Ss], -1]).
+cell_convert_xpiece_to_spiece([Ss, -1], [Ss, -1]) :- !.
+cell_convert_xpiece_to_spiece([Ss, 0], [Ss, -1]) :- !.
+cell_convert_xpiece_to_spiece([[], X], [[X], -1]) :- !.
+cell_convert_xpiece_to_spiece([[X | Ss], X], [[X | Ss], -1]) :- !.
 cell_convert_xpiece_to_spiece([Ss, X], [[X | Ss], -1]).
 
 cell_set_top_piece([Bases, _], Value, [Bases, Value]).
